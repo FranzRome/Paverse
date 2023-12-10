995c5f9fa5af858b77d6667886f094fef80cede1
Franz <francesco.romeo-work@libero.it>
2023-12-10 18:06:12 +0100

    Started Adapting UI to Design

--- Assets/Scripts/ListManager.cs
+++ Assets/Scripts/ListManager.cs
@@ -7,21 +7,40 @@ using UnityEngine.UI;
 public class ListManager : MonoBehaviour
 {
     public ListElement[] elements;
-    public Manager manager;
     public GameObject elementPrefab;
     public Transform content;
-
+    public GameObject list;
+    public GameObject detail;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach(ListElement el in elements)
+        detail.SetActive(false);
+        list.SetActive(true);
+
+        foreach (ListElement el in elements)
         {
             GameObject elementGameObject = Instantiate(elementPrefab);
             elementGameObject.transform.SetParent(content, false);
             elementGameObject.transform.GetChild(0).GetComponent<Image>().sprite = el.sprite;
             elementGameObject.transform.GetChild(1).GetComponent<TMP_Text>().text = el.name;
-            elementGameObject.GetComponent<Button>().onClick.AddListener(() => { manager.SelectElement(el); });
+            elementGameObject.GetComponent<Button>().onClick.AddListener(() => { SelectElement(el); });
         }
+    }
+
+    public void SelectElement(ListElement element)
+    {
+        list.SetActive(false);
+        detail.SetActive(true);
+
+        GameObject.Find("Detail Name").GetComponent<TMP_Text>().text = element.name;
+        GameObject.Find("Detail Image").GetComponent<Image>().sprite = element.sprite;
+        GameObject.Find("Detail Description").GetComponent<TMP_Text>().text = element.description;
+    }
+
+    public void GoToList()
+    {
+        detail.SetActive(false);
+        list.SetActive(true);
     }
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -3,32 +3,13 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
-
-    public GameObject list;
-    public GameObject detail;
-
-    void Start()
+    public void ChangeScene(string sceneName)
     {
-        detail.SetActive(false);
-        list.SetActive(true);
-    }
-
-    public void SelectElement(ListElement element)
-    {
-        list.SetActive(false);
-        detail.SetActive(true);
-
-        GameObject.Find("Detail Name").GetComponent<TMP_Text>().text = element.name;
-        GameObject.Find("Detail Image").GetComponent<Image>().sprite = element.sprite;
-        GameObject.Find("Detail Description").GetComponent<TMP_Text>().text = element.description;
-    }
-
-    public void GoToList()
-    {
-        detail.SetActive(false);
-        list.SetActive(true);
+        Debug.Log("Changing Scene");
+        SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Additive);
     }
 }
