75f4deff974e165e811313ff0e8c21168b1e8ccd
Franz <francesco.romeo-work@libero.it>
2023-12-22 17:54:47 +0100

    Scene Naming + Fixes + Prefabs Update + XR Script Part Removal

--- Assets/SCRIPT/ParticleOnGrab.cs
+++ Assets/SCRIPT/ParticleOnGrab.cs
@@ -1,27 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class ParticleOnGrab : MonoBehaviour
 {
     public ParticleSystem smokeParticles;
 
-    private XRGrabInteractable grabInteractable;
-
     void Start()
     {
-        grabInteractable = GetComponent<XRGrabInteractable>();
-        grabInteractable.onSelectEntered.AddListener(StartSmoke);
-        grabInteractable.onSelectExited.AddListener(StopSmoke);
+
     }
 
-    void StartSmoke(XRBaseInteractor interactor)
+    void StartSmoke()
     {
         smokeParticles.Play();
     }
 
-    void StopSmoke(XRBaseInteractor interactor)
+    void StopSmoke()
     {
         smokeParticles.Stop();
     }
