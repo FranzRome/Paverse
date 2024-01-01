14d0b259fd0954c369615fcc6886d1c7a7f1b83c
ZhengWenJun01 <126774168+ZhengWenJun01@users.noreply.github.com>
2024-01-01 15:17:27 +0100

    AR added back button and escape
    back button and escape to return to main menu

--- Assets/Scripts/AR Scripts/ARManager.cs
+++ Assets/Scripts/AR Scripts/ARManager.cs
@@ -1,27 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 using UnityEngine.SceneManagement;
 
-public class ARManager : MonoBehaviour
+public class PlaneObjectManager : MonoBehaviour
 {
-    float _rotationSpeed = 200f;
+    public GameObject spawn_sedia;
+    public GameObject spawn_porta;
+
+    string spawn_prefab;
+
+    GameObject xr_origin;
+
+    GameObject spawned_object;
+    bool object_spawned;
+    ARRaycastManager arrayman;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xr_origin = GameObject.Find("XR Origin");
+        Debug.Log(xr_origin);
+        object_spawned = false;
+        arrayman = xr_origin.GetComponent<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ChangeScene("Main Menu");
+        }
+    }
+
+    // Update is called once per frame
+    public void SpawnObject()
+    {
+        if (Input.touchCount > 0)
+        {
+            if(spawn_prefab == "spawn_sedia")
+            {
+                SpawnObjectHelper(spawn_sedia);
+            }
+            else if(spawn_prefab == "spawn_porta")
+            {
+                SpawnObjectHelper(spawn_porta);
+            }
+
+            
+        }
+    }
+
+    public void ChangeSpawnPrefab(string prefab)
+    {
+        spawn_prefab = prefab;
+    }
+
+
+    public void SpawnObjectHelper(GameObject prefab)
+    {
+        if (arrayman.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitpose = hits[0].pose;
+            if (!object_spawned)
+            {
+                spawned_object = Instantiate(prefab, hitpose.position, hitpose.rotation);
+                object_spawned = true;
+
+            }
+            else
+            {
+                Destroy(spawned_object);
+                spawned_object = Instantiate(prefab, hitpose.position, hitpose.rotation);
+            }
+        }
+    }
 
     public void ChangeScene(string sceneName)
     {
-        //SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene(), UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
         SceneManager.LoadScene(sceneName);
     }
 
-    public void GiraDestra()
-    {
-        GameObject.Find("ListaStanze").transform.Rotate(0, -_rotationSpeed * Time.deltaTime, 0);
-        
-    }
-
-    public void GiraSinistra()
-    {
-        GameObject.Find("ListaStanze").transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
-
-    }
 }
