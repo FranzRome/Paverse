dc757555844e13efcde57c2ec925b167768e77ce
Franz <francesco.romeo-work@libero.it>
2024-01-15 21:11:54 +0100

    Audio Overlapping Fix

--- Assets/Scripts/ShowObjectOnCollision.cs
+++ Assets/Scripts/ShowObjectOnCollision.cs
@@ -6,6 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     // L'oggetto da far apparire/disparire
     public GameObject objectToShow;
+    public GameObject[] objectsToHide;
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -16,6 +17,12 @@ public class NewBehaviourScript : MonoBehaviour
         {
             // Attiva o disattiva l'oggetto da far apparire/disparire
             objectToShow.SetActive(true);
+
+            // Disattiva tutti gli oggetti da far sparire
+            foreach(GameObject go in objectsToHide)
+            {
+                go.SetActive(false);
+            }
         }
     }
 }
