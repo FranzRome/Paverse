3b7824064a8fd0eb084092470e7d0c2e5a9244f6
Franz <francesco.romeo-work@libero.it>
2024-06-13 17:26:07 +0200

    Design & Code Improvements

---
+++ script.js
@@ -0,0 +1,13 @@
+// Detect when an object is shown and play the transition
+const observer = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      //console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+         setTimeout(() => { entry.target.classList.add('fast-transition'); }, 1000)
+      }
+   });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
