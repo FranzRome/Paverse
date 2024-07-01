e33104436fe28aadc2b040a9c159e9f091c79657
Franz <francesco.romeo-work@libero.it>
2024-07-01 09:17:13 +0200

    Changed Build Settings for Web

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "9 Studio-Paverse-0.9";
+const contentToCache = [
+    "Build/Paverse Web.loader.js",
+    "Build/Paverse Web.framework.js.gz",
+    "Build/Paverse Web.data.gz",
+    "Build/Paverse Web.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
