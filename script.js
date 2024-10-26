69acca0e921cd42282c6e78a28ac652d5f8224aa
Franz <francesco.romeo-work@libero.it>
2024-10-26 10:21:18 +0200

    Improved User Agent Search

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 // Parallax effect
-if(navigator.userAgent === 'Mozilla/5.0 (iPhone13,2; U; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/15E148 Safari/602.1' ||
-   navigator.userAgent === 'Mozilla/5.0 (iPhone12,1; U; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/15E148 Safari/602.1'
-) {
-   console.log('iPhone12');
+console.log(navigator.userAgent);
+
+if(navigator.userAgent.includes('Safari')) {
+   console.log('Safari');
    setInterval(() => {
       document.body.style.backgroundPosition = '0%' + window.scrollY/500 +'%';
    },
