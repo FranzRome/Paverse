62cad409e2d969d7de14f96540fb30c131b20c7b
Franz <francesco.romeo-work@libero.it>
2024-10-26 10:13:14 +0200

    Parallax Effect iPhoneUserAgent

--- script.js
+++ script.js
@@ -13,8 +13,21 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 // Parallax effect
-setInterval(() => {
-   document.body.style.backgroundPosition = '0%' + window.scrollY/3 +'%';
-},
-50
-);
+if(navigator.userAgent === 'Mozilla/5.0 (iPhone13,2; U; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/15E148 Safari/602.1' ||
+   navigator.userAgent === 'Mozilla/5.0 (iPhone12,1; U; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/15E148 Safari/602.1'
+) {
+   console.log('iPhone12');
+   setInterval(() => {
+      document.body.style.backgroundPosition = '0%' + window.scrollY/70 +'%';
+   },
+   5
+   );
+} else {
+   console.log('other device');
+   
+   setInterval(() => {
+      document.body.style.backgroundPosition = '0%' + window.scrollY/5 +'%';
+   },
+   30
+   );
+}
